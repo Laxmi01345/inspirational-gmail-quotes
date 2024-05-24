ed84f3edbcad6fa510b190f142a0e5071fd6de91
laxmi013 <laxmiray013@gmail.com>
2024-05-24 21:24:35 +0530

    fourth commit

--- Server/index.js
+++ Server/index.js
@@ -17,7 +17,7 @@ const PORT = 4000;
 
 app.use(express.json());
 app.use(cors({
-    origin: ["http://localhost:5173"],
+    origin: ["https://quotegeneratorgmail.netlify.app/"],
     methods: ["GET", "POST"],
     credentials: true
 }));
